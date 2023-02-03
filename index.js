4649ab397d696701f049e3641de25e2575c3456e
Bernhard Hustomo <bernhard.hustomo@gmail.com>
2023-02-03 15:46:32 +0800

    first four variables declared

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-const
+const currencyElementOne = document.getElementById("currency-one");
+const currencyElementTwo = document.getElementById("currency-two");
+const amountElementOne = document.getElementById("amount-one");
+const amountElementTwo = document.getElementById("amount-two");
