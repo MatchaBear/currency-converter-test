d38a7085a6d078c6c1e0a14a07b53877a87bbf5c
Bernhard Hustomo <bernhard.hustomo@gmail.com>
2023-02-03 16:27:05 +0800

    getting used to it

--- index.js
+++ index.js
@@ -2,3 +2,17 @@ const currencyElementOne = document.getElementById("currency-one");
 const currencyElementTwo = document.getElementById("currency-two");
 const amountElementOne = document.getElementById("amount-one");
 const amountElementTwo = document.getElementById("amount-two");
+
+const rateElement = document.getElementById("rate");
+// 'rate' is under the button
+
+const swap = document.getElementById('swap');
+// 'swap' is the button
+
+function calculate() {
+    const currencyOne = currencyElementOne.value;
+    const currencyTwo = currencyElementTwo.value;
+
+fetch(`https://api.exchangerate.host/convert?from=`+${currencyOne});
+
+}
